f508426f27b4ff965711f7ac6a99b6db0c48270c
keiiwami623 <kei.iwami.623@gmail.com>
2024-01-28 22:03:38 +0900

    s

---
+++ index.js
@@ -0,0 +1,14 @@
+const Fastify = require("fastify");
+const server = Fastify();
+
+server.register(require("@fastify/http-proxy"), {
+  upstream: "https://job.rikunabi.com/",
+  replyOptions: {
+    onResponse: (request, reply, res) => {
+      reply.removeHeader("X-Frame-Options");
+      reply.send(res)
+    },
+  },
+});
+
+server.listen({ port: 3000 });
